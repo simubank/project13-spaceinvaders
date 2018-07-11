22d3a3f5b773e1229b7c84d74c7244e20f9a860e
Alex Nguyen <alexnguyen@live.ca>
2018-07-11 10:10:39 -0600

    initial commit

---
+++ app/src/main/java/ca/td/tdmylevelup/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package ca.td.tdmylevelup
+
+import android.app.Activity
+import android.os.Bundle
+
+class DashboardActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
