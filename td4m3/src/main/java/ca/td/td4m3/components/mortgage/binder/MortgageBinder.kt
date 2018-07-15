52eb839e92c3e3de7222fbc361b36df1a2bb6fc6
Rishi-B <33183390+Rishi-B@users.noreply.github.com>
2018-07-15 14:21:36 -0400

    Address comment

--- td4m3/src/main/java/ca/td/td4m3/components/mortgage/binder/MortgageBinder.kt
+++ td4m3/src/main/java/ca/td/td4m3/components/mortgage/binder/MortgageBinder.kt
@@ -1,19 +1,21 @@
 package ca.td.td4m3.components.mortgage.binder
 
+import android.support.annotation.DrawableRes
 import android.view.View
 import android.view.ViewGroup
 import ca.td.td4m3.R
 import ca.td.td4m3.components.mortgage.viewholder.MortgageViewHolder
 import com.ngam.rvabstractions.AbstractDataBinder
 
-class MortgageBinder(private val listener: View.OnClickListener): AbstractDataBinder<MortgageViewHolder>() {
+class MortgageBinder(@DrawableRes private val drawableId: Int,
+                     private val listener: View.OnClickListener?): AbstractDataBinder<MortgageViewHolder>() {
 
     override fun createViewHolder(parent: ViewGroup): MortgageViewHolder {
         return MortgageViewHolder(getView(MortgageViewHolder.getLayoutId(), parent))
     }
 
     override fun bindViewHolder(viewHolder: MortgageViewHolder) {
-        viewHolder.imageView.setBackgroundResource(R.drawable.mortgage_picture)
+        viewHolder.imageView.setBackgroundResource(drawableId)
         viewHolder.imageView.invalidate()
         viewHolder.imageView.setOnClickListener(listener)
     }
--- td4m3/src/main/java/ca/td/td4m3/dashboard/mortgage/adapter/MortgageAdapter.kt
+++ td4m3/src/main/java/ca/td/td4m3/dashboard/mortgage/adapter/MortgageAdapter.kt
@@ -1,5 +1,6 @@
 package ca.td.td4m3.dashboard.mortgage.adapter
 
+import ca.td.td4m3.R
 import ca.td.td4m3.components.mortgage.binder.MortgageBinder
 import ca.td.td4m3.dashboard.mortgage.presenter.MortgagePresenter
 import com.ngam.rvabstractions.AbstractDataBindAdapter
@@ -7,6 +8,6 @@ import com.ngam.rvabstractions.AbstractDataBindAdapter
 class MortgageAdapter(val presenter: MortgagePresenter): AbstractDataBindAdapter() {
 
     override fun buildRows() {
-        add(MortgageBinder(presenter.createOnClickListener()))
+        add(MortgageBinder(R.drawable.mortgage_picture, presenter.createOnClickListener()))
     }
 }
