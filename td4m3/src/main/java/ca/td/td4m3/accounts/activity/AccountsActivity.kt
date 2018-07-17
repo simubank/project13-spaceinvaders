5266ec92b2e991232d14cbe272ebc0a546d73969
Dave Gumba <dave_gumba@Yahoo.com>
2018-07-17 17:51:23 -0400

    added subclassing

--- td4m3/src/main/java/ca/td/td4m3/accounts/activity/AccountsActivity.kt
+++ td4m3/src/main/java/ca/td/td4m3/accounts/activity/AccountsActivity.kt
@@ -1,4 +1,12 @@
 package ca.td.td4m3.accounts.activity
 
-class AccountsActivity {
+import ca.td.td4m3.survey.adapter.SurveyAdapter
+import ca.td.td4m3.survey.presenter.SurveyPresenter
+import com.ngam.rvabstractions.AbstractActivity
+import com.ngam.rvabstractions.AbstractClassProperties
+
+class AccountsActivity : AbstractActivity<SurveyPresenter, SurveyAdapter>() {
+    override fun setProperties(): AbstractClassProperties<SurveyPresenter, SurveyAdapter> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
--- td4m3/src/main/java/ca/td/td4m3/accounts/adapter/AccountsAdapter.kt
+++ td4m3/src/main/java/ca/td/td4m3/accounts/adapter/AccountsAdapter.kt
@@ -1,4 +1,9 @@
 package ca.td.td4m3.accounts.adapter
 
-class AccountsAdapter {
+import com.ngam.rvabstractions.AbstractDataBindAdapter
+
+class AccountsAdapter : AbstractDataBindAdapter() {
+    override fun buildRows() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
