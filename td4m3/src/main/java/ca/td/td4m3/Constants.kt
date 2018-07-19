fb8bc01e6cd2217469d3233f780e5b5aee1e32e8
nishatoma <nishatoma@gmail.com>
2018-07-18 23:12:04 -0400

    garbage

--- td4m3/src/main/java/ca/td/td4m3/Constants.kt
+++ td4m3/src/main/java/ca/td/td4m3/Constants.kt
@@ -3,7 +3,7 @@ package ca.td.td4m3
 class Constants private constructor() {
 
     companion object {
-        val SHARED_PREFERENCES : String = ""
-        val IS_FIRST_TIME : String = "isFirstTime"
+        const val AUTH_TOKEN : String = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiMjgxMzgzOCIsImV4cCI6OTIyMzM3MjAzNjg1NDc3NSwiYXBwX2lkIjoiMWY5ZTA4OTAtNzdiNS00NGFlLWE5ODctMDNhMGE2YTEwMjlhIn0.FEdf6MQ1XLVFaaV3V6Ocf3aAcGXksANva2K_YTUz2EA"
+        const val ID = "1f9e0890-77b5-44ae-a987-03a0a6a1029a_6c8434d3-9d00-45d9-83d6-5c87cc97cdd8"
     }
 }
